9e9d6568294a3b86aadcfc3bfc68a8f58585bd7e
Daniel Beskin <daniel.beskin@gmail.com>
2015-03-19 12:38:45 +0200

    Adding JavaScript for tweet embedding

---
+++ code/twitter.js
@@ -0,0 +1,23 @@
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+ 
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+ 
+  return t;
+}(document, "script", "twitter-wjs"));
+
+window.addEventListener('load', function(){
+     var tweets = document.getElementsByClassName("tweet");
+   Array.prototype.forEach.call(tweets, function(t) {
+     twttr.widgets.createTweet(t.id, t)
+   })
+});
